ad49352cfeee02771ccc66697979c9a4d3f222c9
Korzikowski,Yannik <yannik@korzikowski.de>
2019-03-18 01:45:30 +0100

    added missing quotes

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "no" | avdmanager create avd -f -n test -k "system-images;android-${API_LEVEL};google_apis;x86_64" --device ${DEVICE_ID}
+echo "no" | avdmanager create avd -f -n test -k "system-images;android-${API_LEVEL};google_apis;x86_64" --device "${DEVICE_ID}"
 emulator64-x86 -avd test -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet &
